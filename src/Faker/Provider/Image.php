b49cc437a1f556e60da9d1715494c9d6d132645f
Eddy <85066311+E-Bousk@users.noreply.github.com>
2022-01-11 17:49:11 +0100

    Manage redirection on picsum.photos
    ie : URL changes from « https://picsum.photos/id/237/640/480.jpg » to « https://i.picsum.photos/id/237/640/480.jpg?hmac=WUvxmj5giHkcHhGASm19RfCwScxNCvBvlABvSbbeNYQ »

--- src/Faker/Provider/Image.php
+++ src/Faker/Provider/Image.php
@@ -101,9 +101,18 @@ class Image extends Base
         if (function_exists('curl_exec')) {
             // use cURL
             $fp = fopen($filepath, 'w');
-            $ch = curl_init($url);
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_exec($ch);
+
+            $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
             curl_setopt($ch, CURLOPT_FILE, $fp);
+
             $success = curl_exec($ch) && curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200;
+
             fclose($fp);
             curl_close($ch);
 
